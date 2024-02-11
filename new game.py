aa56fa8d89852fd7e2b18f72ed5ad5eb90896693
Yavorski Andri <aandrij074@gmai.com>
2024-02-11 19:43:56 +0200

    first commit

---
+++ new game.py
@@ -0,0 +1,10 @@
+import pygame
+import time
+from random import randint
+pygame.init()
+
+back=(200, 255, 255) 
+mw = pygame.display.set_mode((500,500))
+mw.fill(back)
+clock = pygame.time.Clock()
+
